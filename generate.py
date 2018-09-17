4c35ea43f136d33fddefc9d0fa451e547b2cdfec
Jonas Bakken <jonasxb@gmail.com>
2018-09-17 15:12:25 +0200

    Allow multiple pdfs to be generated at the same time

--- generate.py
+++ generate.py
@@ -3,8 +3,20 @@ from subprocess import call
 import json
 import os
 import sys
+import hashlib
+import time
 
-def addImages(tex, images):
+def mkdir(out):
+    call(["mkdir", out])
+    call(["cp", "-r", "images", out])
+
+def rmdir(out):
+    call(["rm", "-r", out])
+
+def get_hash(json):
+    return hashlib.md5(bytes(json+str(time.time()), "utf-8")).hexdigest()
+
+def add_images(tex, images):
     i = 0
     embedded = ""
     immediate = ""
@@ -19,26 +31,34 @@ def addImages(tex, images):
     tex = tex.replace("%EMBEDDED_GRAPHICS%", graphics)
     return tex
 
-def generateTex(values):
+def generate_tex(values, directory):
     tex = ""
     with open("template.tex", "r") as f:
         tex = "".join(f.readlines())
     for field, value in values.items():
-        tex = addImages(tex, value) if field == "images" else tex.replace("%{0}%".format(field.upper()), value)
+        tex = add_images(tex, value) if field == "images" else tex.replace("%{0}%".format(field.upper()), value)
     tex = tex.replace("%RECEIPT%", "{0} vedlegg".format(len(values.get("images") or [])))
-    with open("out.tex", "w") as f:
+    with open("{0}/out.tex".format(directory), "w") as f:
         f.write(tex)
 
-def generatePdf():
+def generate_pdf():
     call(["pdflatex", "--shell-escape", "out.tex"], stdout=open(os.devnull, 'wb'))
 
 def handle(req):
+    directory = get_hash(req)
+    mkdir(directory)
+
     body = json.loads(req)
 
-    generateTex(body)
-    generatePdf()
+    generate_tex(body, directory)
+
+    os.chdir(directory)
+    generate_pdf()
     with open("out.pdf", "rb") as f:
         sys.stdout.buffer.write(f.read())
+    os.chdir("/app")
+
+    rmdir(directory)
 
 def get_stdin():
     buf = ""
