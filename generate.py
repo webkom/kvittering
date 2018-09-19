f07e77218737799268d51db6c740014bfe993dbe
Jonas Bakken <jonasxb@gmail.com>
2018-09-19 22:06:02 +0200

    Allow uploading of signature image, fix bug with too big images, layout

--- generate.py
+++ generate.py
@@ -5,6 +5,7 @@ import os
 import sys
 import hashlib
 import time
+import math
 
 def mkdir(out):
     call(["mkdir", out])
@@ -16,19 +17,34 @@ def rmdir(out):
 def get_hash(json):
     return hashlib.md5(bytes(json+str(time.time()), "utf-8")).hexdigest()
 
+def shorten_line_length(string):
+    max_len = 1000
+    return "\n".join([string[i*max_len:(i+1)*max_len] for i in range(0, math.ceil(len(string)/max_len))])
+
 def add_images(tex, images):
     i = 0
     embedded = ""
     immediate = ""
     graphics = ""
     for image in images:
-       embedded += "\\begin{{filecontents*}}{{\jobname.embedded{0}}}\n{1}\n\\end{{filecontents*}}\n".format(i, image)
-       immediate += "\\immediate\\write18{{base64 -d \\jobname.embedded{0} > \\jobname-tmp{0}.pdf}}\n".format(i)
-       graphics += "\\newpage\n\\fbox{{\\includegraphics[width=3cm]{{\\jobname-tmp{0}.pdf}}}}".format(i)
-       i += 1
+        image = shorten_line_length(image)
+        embedded += "\\begin{{filecontents*}}{{\jobname.embedded{0}}}\n{1}\n\\end{{filecontents*}}\n".format(i, image)
+        immediate += "\\immediate\\write18{{base64 -d \\jobname.embedded{0} > \\jobname-tmp{0}.pdf}}\n".format(i)
+        graphics += "\\newpage\n\\fbox{{\\includegraphics[width=\\textwidth,height=\\textheight,keepaspectratio]{{\\jobname-tmp{0}.pdf}}}}".format(i)
+        i += 1
     tex = tex.replace("%EMBEDDED_IMAGES%", embedded)
     tex = tex.replace("%EMBEDDED_IMMEDIATE%", immediate)
     tex = tex.replace("%EMBEDDED_GRAPHICS%", graphics)
+    return tex
+
+def add_signature(tex, signature):
+    signature = shorten_line_length(signature)
+    embedded = "\\begin{{filecontents*}}{{\jobname.embedded{0}}}\n{1}\n\\end{{filecontents*}}\n".format("signature", signature)
+    immediate = "\\immediate\\write18{{base64 -d \\jobname.embedded{0} > \\jobname-tmp{0}.pdf}}\n".format("signature")
+    graphics = "\\newpage\n\\fbox{{\\includegraphics[width=6cm]{{\\jobname-tmp{0}.pdf}}}}".format("signature")
+    tex = tex.replace("%EMBEDDED_SIGNATURE%", embedded)
+    tex = tex.replace("%SIGNATURE_IMMEDIATE%", immediate)
+    tex = tex.replace("%SIGNATURE%", graphics)
     return tex
 
 def generate_tex(values, directory):
@@ -36,7 +52,12 @@ def generate_tex(values, directory):
     with open("template.tex", "r") as f:
         tex = "".join(f.readlines())
     for field, value in values.items():
-        tex = add_images(tex, value) if field == "images" else tex.replace("%{0}%".format(field.upper()), value)
+        if field == "images":
+            tex = add_images(tex, value)
+        elif field == "signature":
+            tex = add_signature(tex, value)
+        else:
+            tex = tex.replace("%{0}%".format(field.upper()), value)
     tex = tex.replace("%RECEIPT%", "{0} vedlegg".format(len(values.get("images") or [])))
     with open("{0}/out.tex".format(directory), "w") as f:
         f.write(tex)
