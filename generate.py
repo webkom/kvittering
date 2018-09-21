010fdb918ed2d479642e4cef22f41f59cfa0e5f6
Jonas Bakken <jonasxb@gmail.com>
2018-09-21 13:02:40 +0200

    Send mail with pdf and tex as attachments if wanted, yapf.

--- generate.py
+++ generate.py
@@ -1,4 +1,3 @@
-from http.server import BaseHTTPRequestHandler, HTTPServer
 from subprocess import call
 import json
 import os
@@ -6,64 +5,85 @@ import sys
 import hashlib
 import time
 import math
+import mail
+
 
 def mkdir(out):
-    call(["mkdir", out])
-    call(["cp", "-r", "images", out])
+    call(['mkdir', out])
+    call(['cp', '-r', 'images', out])
+
 
 def rmdir(out):
-    call(["rm", "-r", out])
+    call(['rm', '-r', out])
+
 
 def get_hash(json):
-    return hashlib.md5(bytes(json+str(time.time()), "utf-8")).hexdigest()
+    return hashlib.md5(bytes(json + str(time.time()), 'utf-8')).hexdigest()
+
 
 def shorten_line_length(string):
     max_len = 1000
-    return "\n".join([string[i*max_len:(i+1)*max_len] for i in range(0, math.ceil(len(string)/max_len))])
+    return '\n'.join([
+        string[i * max_len:(i + 1) * max_len]
+        for i in range(0, math.ceil(len(string) / max_len))
+    ])
+
 
 def add_images(tex, images):
     i = 0
-    embedded = ""
-    immediate = ""
-    graphics = ""
+    embedded = ''
+    immediate = ''
+    graphics = ''
     for image in images:
         image = shorten_line_length(image)
-        embedded += "\\begin{{filecontents*}}{{\jobname.embedded{0}}}\n{1}\n\\end{{filecontents*}}\n".format(i, image)
-        immediate += "\\immediate\\write18{{base64 -d \\jobname.embedded{0} > \\jobname-tmp{0}.pdf}}\n".format(i)
-        graphics += "\\newpage\n\\fbox{{\\includegraphics[width=\\textwidth,height=\\textheight,keepaspectratio]{{\\jobname-tmp{0}.pdf}}}}".format(i)
+        embedded += '\\begin{{filecontents*}}{{\jobname.embedded{0}}}\n{1}\n\\end{{filecontents*}}\n'.format(
+            i, image)
+        immediate += '\\immediate\\write18{{base64 -d \\jobname.embedded{0} > \\jobname-tmp{0}.pdf}}\n'.format(
+            i)
+        graphics += '\\newpage\n\\fbox{{\\includegraphics[width=\\textwidth,height=\\textheight,keepaspectratio]{{\\jobname-tmp{0}.pdf}}}}'.format(
+            i)
         i += 1
-    tex = tex.replace("%EMBEDDED_IMAGES%", embedded)
-    tex = tex.replace("%EMBEDDED_IMMEDIATE%", immediate)
-    tex = tex.replace("%EMBEDDED_GRAPHICS%", graphics)
+    tex = tex.replace('%EMBEDDED_IMAGES%', embedded)
+    tex = tex.replace('%EMBEDDED_IMMEDIATE%', immediate)
+    tex = tex.replace('%EMBEDDED_GRAPHICS%', graphics)
     return tex
+
 
 def add_signature(tex, signature):
     signature = shorten_line_length(signature)
-    embedded = "\\begin{{filecontents*}}{{\jobname.embedded{0}}}\n{1}\n\\end{{filecontents*}}\n".format("signature", signature)
-    immediate = "\\immediate\\write18{{base64 -d \\jobname.embedded{0} > \\jobname-tmp{0}.pdf}}\n".format("signature")
-    graphics = "\\newpage\n\\fbox{{\\includegraphics[width=6cm]{{\\jobname-tmp{0}.pdf}}}}".format("signature")
-    tex = tex.replace("%EMBEDDED_SIGNATURE%", embedded)
-    tex = tex.replace("%SIGNATURE_IMMEDIATE%", immediate)
-    tex = tex.replace("%SIGNATURE%", graphics)
+    embedded = '\\begin{{filecontents*}}{{\jobname.embedded{0}}}\n{1}\n\\end{{filecontents*}}\n'.format(
+        'signature', signature)
+    immediate = '\\immediate\\write18{{base64 -d \\jobname.embedded{0} > \\jobname-tmp{0}.pdf}}\n'.format(
+        'signature')
+    graphics = '\\newpage\n\\fbox{{\\includegraphics[width=6cm]{{\\jobname-tmp{0}.pdf}}}}'.format(
+        'signature')
+    tex = tex.replace('%EMBEDDED_SIGNATURE%', embedded)
+    tex = tex.replace('%SIGNATURE_IMMEDIATE%', immediate)
+    tex = tex.replace('%SIGNATURE%', graphics)
     return tex
 
+
 def generate_tex(values, directory):
-    tex = ""
-    with open("template.tex", "r") as f:
-        tex = "".join(f.readlines())
+    tex = ''
+    with open('template.tex', 'r') as f:
+        tex = ''.join(f.readlines())
     for field, value in values.items():
-        if field == "images":
+        if field == 'images':
             tex = add_images(tex, value)
-        elif field == "signature":
+        elif field == 'signature':
             tex = add_signature(tex, value)
         else:
-            tex = tex.replace("%{0}%".format(field.upper()), value)
-    tex = tex.replace("%RECEIPT%", "{0} vedlegg".format(len(values.get("images") or [])))
-    with open("{0}/out.tex".format(directory), "w") as f:
+            tex = tex.replace('%{0}%'.format(field.upper()), value)
+    tex = tex.replace('%RECEIPT%', '{0} vedlegg'.format(
+        len(values.get('images') or [])))
+    with open('{0}/out.tex'.format(directory), 'w') as f:
         f.write(tex)
 
+
 def generate_pdf():
-    call(["pdflatex", "--shell-escape", "out.tex"], stdout=open(os.devnull, 'wb'))
+    call(['pdflatex', '--shell-escape', 'out.tex'],
+         stdout=open(os.devnull, 'wb'))
+
 
 def handle(req):
     directory = get_hash(req)
@@ -75,18 +95,24 @@ def handle(req):
 
     os.chdir(directory)
     generate_pdf()
-    with open("out.pdf", "rb") as f:
+    with open('out.pdf', 'rb') as f:
         sys.stdout.buffer.write(f.read())
-    os.chdir("/app")
+
+    if 'mailto' in body and len(body['mailto']) > 0:
+        mail.send_mail(body['mailto'], body, ['out.tex', 'out.pdf'])
+
+    os.chdir('/app')
 
     rmdir(directory)
 
+
 def get_stdin():
-    buf = ""
+    buf = ''
     for line in sys.stdin:
         buf = buf + line
     return buf
 
-if __name__ == "__main__":
+
+if __name__ == '__main__':
     st = get_stdin()
     handle(st)
---
+++ mail.py
@@ -0,0 +1,39 @@
+import smtplib
+import os
+from os.path import basename
+from email.mime.application import MIMEApplication
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.utils import formatdate
+
+
+def send_mail(mail_to, body, files):
+    mail_from = os.environ['MAIL_ADDRESS']
+    mail_password = os.environ['MAIL_PASSWORD']
+
+    msg = MIMEMultipart()
+    msg['From'] = mail_from
+    msg['To'] = mail_to
+    msg['Date'] = formatdate(localtime=True)
+    msg['Subject'] = 'Kvitteringsskildring fra {0}'.format(body['name'])
+
+    text = 'Laget av: {0}\n'.format(body.get('name', ''))
+    text += 'Annledning: {0}\n'.format(body.get('occasion', ''))
+    text += 'Beløp: {0}\n'.format(body.get('amount', ''))
+    text += 'Kommentar: {0}\n'.format(body.get('comment', ''))
+    text += 'Kvitteringsskildring er lagt ved i pdf og tex format'
+
+    msg.attach(MIMEText(text))
+
+    for f in files or []:
+        with open(f, 'rb') as fil:
+            part = MIMEApplication(fil.read(), Name=basename(f))
+        part['Content-Disposition'] = 'attachment; filename="{0}"'.format(
+            basename(f))
+        msg.attach(part)
+
+    server = smtplib.SMTP('smtp.gmail.com', 587)
+    server.starttls()
+    server.login(mail_from, mail_password)
+    server.sendmail(mail_from, mail_to, msg.as_string())
+    server.close()
