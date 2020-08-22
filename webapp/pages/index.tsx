98bbf0dac59a7bb3b056efbc2c773ea870abff6a
Peder Smith <smith.peder@gmail.com>
2020-08-22 15:41:12 +0200

    Fix some ugly css on the header

--- webapp/pages/index.tsx
+++ webapp/pages/index.tsx
@@ -7,11 +7,12 @@ const App = (): JSX.Element => (
   <React.Fragment>
     <CssBaseline />
     <Container maxWidth="sm" fixed>
-      <div
-        style={{ display: 'flex', justifyContent: 'center', padding: '30px' }}
-      >
-        <img src="/abakus.png" alt="abakus logo" width="250px" />
-      </div>
+      <img
+        src="/abakus.png"
+        alt="abakus logo"
+        width="100%"
+        style={{ padding: '50px' }}
+      />
       <Form />
     </Container>
   </React.Fragment>
