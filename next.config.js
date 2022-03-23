5359f903be196061aa250184ba6a50b308ce41ad
Peder Smith <smith.peder@gmail.com>
2022-03-23 13:18:44 +0100

    Fix next optimized imgaes for local export

--- next.config.js
+++ next.config.js
@@ -1,15 +1,19 @@
-const path = require("path");
+const path = require('path');
+const withPlugins = require('next-compose-plugins');
+const optimizedImages = require('next-optimized-images');
 
 module.exports = {
   env: {
-    API_URL: process.env.API_URL
+    API_URL: process.env.API_URL,
   },
   webpack(config, options) {
-    config.resolve.alias["components"] = path.join(
+    config.resolve.alias['components'] = path.join(
       __dirname,
-      "webapp/components"
+      'webapp/components'
     );
-    config.resolve.alias["utils"] = path.join(__dirname, "webapp/utils");
+    config.resolve.alias['utils'] = path.join(__dirname, 'webapp/utils');
     return config;
-  }
+  },
 };
+
+module.exports = withPlugins([[optimizedImages, {}]]);
