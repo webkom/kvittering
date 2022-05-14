e899f501b63584772609fc9392f2d07c59f144a2
Ivar Nakken <ivarnakken1441@gmail.com>
2022-05-14 20:07:37 +0200

    Cleanup on aisle 5

--- webapp/pages/index.tsx
+++ webapp/pages/index.tsx
@@ -1,22 +1,26 @@
-import { Container, Card, Row, Text } from '@nextui-org/react';
+import { Card, Container, Divider, Row, Spacer, Text } from '@nextui-org/react';
 
 import Form from 'components/Form';
 import Footer from 'components/Footer';
 
 const App = (): JSX.Element => (
-  <>
-    <Container fluid xs>
-      <Card>
+  <Container xs>
+    <Card>
+      <Card.Header>
         <Row justify="center" align="center">
           {/* eslint-disable-next-line @next/next/no-img-element */}
           <img src="/favicon.png" style={{ width: '50px' }} />
+          <Spacer x={0.4} />
           <Text h1>Kvitteringsskjema</Text>
         </Row>
+      </Card.Header>
+      <Divider />
+      <Card.Body>
         <Form />
-      </Card>
-    </Container>
+      </Card.Body>
+    </Card>
     <Footer />
-  </>
+  </Container>
 );
 
 export default App;
