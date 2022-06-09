d6c9b2602bd4f792ce1290d1d06ba9e62e45bf65
Ivar Nakken <ivarnakken1441@gmail.com>
2022-06-09 15:28:01 +0200

    Migrate to cypress 10

---
+++ cypress.config.ts
@@ -0,0 +1,26 @@
+import { defineConfig } from 'cypress';
+
+export default defineConfig({
+  projectId: 'jitps9',
+  defaultCommandTimeout: 10000,
+  viewportWidth: 1000,
+  viewportHeight: 1300,
+  modifyObstructiveCode: false,
+  chromeWebSecurity: false,
+  retries: {
+    runMode: 2,
+    openMode: 0,
+  },
+  commands: {
+    supportFile: 'webapp/cypress/commands.js',
+  },
+  e2e: {
+    baseUrl: 'http://localhost:3000',
+    specPattern: 'webapp/cypress/e2e/**/*.{js,jsx,ts,tsx}',
+    fixturesFolder: "webapp/cypress/fixtures",
+    downloadsFolder: "webapp/cypress/downloads",
+    screenshotsFolder: "webapp/cypress/screenshots",
+    videosFolder: "webapp/cypress/videos",
+    supportFile: "webapp/cypress/support/e2e.js"
+  },
+});
